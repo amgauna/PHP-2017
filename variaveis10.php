debcced577ca08458fc0d5bec3e4509b8a944f49
Ana Gauna <amgauna@gmail.com>
2022-01-16 18:36:26 -0300

    Create variaveis10.php

---
+++ variaveis10.php
@@ -0,0 +1,19 @@
+<?php
+
+$a = 5;
+echo $a++."<br>";
+echo $a."<br>";
+
+$b = 6;
+echo ++$b."<br>";
+echo $b."<br>";
+
+$c = 7;
+echo $c--."<br>";
+echo $c."<br>";
+
+$d = 8;
+echo --$d."<br>";
+echo $d."<br>";
+
+?>
